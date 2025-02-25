b1496dc8f40e0f80e9dd3b3a254042850c10f119
sveste123 <sverrestenbaek@icloud.com>
2025-02-25 12:38:43 +0100

    disabling dev from @app/environment

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,6 @@
-import { dev } from '@app/environment';
+// import { dev } from '@app/environment';
 import { injectAnalytics } from '@vercel/analytics/sveltekit';
 
 injectAnalytics({ 
-    mode: dev ? 'development' : 'production' 
+    // mode: dev ? 'development' : 'production' 
 });
