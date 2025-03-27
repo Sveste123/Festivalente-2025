a2c527785c37f21b8ae4963146815be0944f7b2f
sveste123 <sverrestenbaek@icloud.com>
2025-03-27 19:45:37 +0100

    greater sharing

--- src/routes/invitasjon/[id]/+page.svelte
+++ src/routes/invitasjon/[id]/+page.svelte
@@ -22,7 +22,7 @@
   // Hent kryptert melding fra URL
   $: encryptedMessage = $page.url.searchParams.get('s') || '';
   $: concert = $page.url.searchParams.get('img') || ''; // Standard til 1 hvis ikke spesifisert
-  $: decryptedMessage = encryptedMessage ? decrypt(decodeURIComponent(encryptedMessage)) : 'error';
+  $: decryptedMessage = encryptedMessage ? decrypt(decodeURIComponent(encryptedMessage)) : '';
 
   async function copyToClipboard() {
     try {
@@ -35,12 +35,30 @@
     }
   }
 
+  const sharePage = async () => {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: document.title,
+          text: "Sjekk ut denne siden!",
+          url: window.location.href
+        });
+      } catch (error) {
+        console.error("Deling mislyktes", error);
+        copyToClipboard
+      }
+    } else {
+      copyToClipboard
+    }
+  };
+
+
 </script>
 
 <!-- <h1>{decryptedMessage}</h1> -->
 <Template sentence={decryptedMessage} concert={concert} />
 
-<a href={$page.url.href} on:click={copyToClipboard}>
+<a href={$page.url.href} on:click={sharePage}>
   <svg xmlns="http://www.w3.org/2000/svg" width="80px" height="80px" viewBox="0 0 24 24" fill="none">
     <path fill-rule="evenodd" clip-rule="evenodd" d="M6 19.0037V8.00368H9V9.50368H7.5V17.5037H16.5V9.50368H15V8.00368H18V19.0037H6Z" fill="#1F2328"/>
     <path fill-rule="evenodd" clip-rule="evenodd" d="M11.25 4.87132L9.47462 6.64669L8.41396 5.58603L12 2L15.586 5.58603L14.5254 6.64669L12.75 4.87132L12.75 12H11.25L11.25 4.87132Z" fill="#1F2328"/>
